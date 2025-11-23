b2b046def9ac30dbfb44121fb9f3961526dbceed
ABD03 <abdulriyas.p786@gmail.com>
2025-11-23 19:51:29 +0530

    update

--- src/app/(website)/login/pageContainer.tsx
+++ src/app/(website)/login/pageContainer.tsx
@@ -1,4 +1,40 @@
 "use client";
+import Image from "next/image";
+import { Button, Form, Input } from "antd";
+import { ImageLoader } from "@/util/imageLoader";
+
 export default function LoginContainer() {
-  return <div>LoginContainer</div>;
+  return (
+    <div className="w-full h-[75vh] md:h-screen flex border-b border-b-gray-100">
+      <div className="hidden sm:block w-1/2 h-[75vh] md:h-screen bg-[url('/bg2.webp')] bg-cover bg-center"></div>
+      <div className="w-full sm:w-1/2 h-[90vh] flex items-center justify-center">
+        <div className="max-w-sm w-full p-3">
+          <Image
+            alt="logo"
+            width={0}
+            height={0}
+            src={"/logo.png"}
+            loader={ImageLoader}
+            priority={true}
+            className="w-20 h-20 object-cover mb-2"
+          />
+          <div className="text-2xl font-bold font-sans">Sign In Now</div>
+          <div className="mb-10 text-gray-500 text-base font-sans">
+            Welcome to Energy Bloom
+          </div>
+          <Form>
+            <Form.Item>
+              <Input placeholder="Enter email" />
+            </Form.Item>
+            <Form.Item>
+              <Input placeholder="Enter password" />
+            </Form.Item>
+            <Button type="primary" size="large" block>
+              Sign In
+            </Button>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,10 +6,21 @@ export default function Footer() {
   return (
     <div className="max-w-7xl mx-auto  p-3 font-sans">
       <div className="flex flex-col md:flex-row gap-4 align-center justify-center mb-3 text-xs">
-        <div><Link href="/">Terms and Conditions</Link></div>
-        <div><Link href="/">Privacy Policy</Link></div>
-        <div><Link href="/">Cookie Policy</Link></div>
-        <div><Link href="/">Contact Us</Link></div>
+        <div>
+          <Link href="/login">Login</Link>
+        </div>
+        <div>
+          <Link href="/">Terms and Conditions</Link>
+        </div>
+        <div>
+          <Link href="/">Privacy Policy</Link>
+        </div>
+        <div>
+          <Link href="/">Cookie Policy</Link>
+        </div>
+        <div>
+          <Link href="/">Contact Us</Link>
+        </div>
       </div>
       <div className="text-center text-xs border-t border-t-indigo-50 pt-3 text-gray-500">
         Copyright © 2010-2025
